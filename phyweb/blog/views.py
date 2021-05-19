53a37d871bb95b09b08cc62e55b2dd96f0694934
Cyberdom123 <dmichalczyk73@gamil.com>
2021-05-19 12:50:34 +0200

    tests

--- phyweb/blog/views.py
+++ phyweb/blog/views.py
@@ -14,15 +14,36 @@ class PostListView(ListView):  #same view based on class, site view with object 
     template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, 
+
+    post = get_object_or_404(Post,
         slug = post, 
         status = 'published',   #str 27 
         publish__year = year,
         publish__month = month,
         publish__day = day)
     #list of active commens for this post
+    sent = False
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # from fields passed validation
+            cd = form.cleaned_data
+            #send email
+            post_url = request.build_absolute_uri(
+                    post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                    f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                    f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com',
+                    [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                    'form': form})
 
 def post_share(request, post_id):
     # Retrive post by id
@@ -50,4 +71,8 @@ def post_share(request, post_id):
                                                     'form':form,
                                                     'sent':sent})
 
+def TestPage(request):
+
+    form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'form':form})
 # Create your views here.
