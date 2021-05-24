f5998733ff9df00a567b975a0568d5019a5997f0
Cyberdom123 <dmichalczyk73@gamil.com>
2021-05-24 23:13:54 +0200

    auth

--- phyweb/login/models.py
+++ phyweb/login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	username = models.CharField(max_length=32)
+	emial = models.EmailField(max_length=254)
+	password = models.CharField(max_length=256)
+
+	def natural_ket(self):
+		return (self.username)
--- phyweb/login/views.py
+++ phyweb/login/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
 from .forms import LoginForm
+from .models import User
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
-def login(request):
-	form = LoginForm()
-	return render(request, 'login/login.html', {'form':form})
+#def login(request):
+#	form = LoginForm()
+#	return render(request, 'login/login.html', {'form':form})
+
+
+def login_view(request):
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			user = authenticate(request,
+				username=cd['login'],
+				password=cd['password'])
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return HttpResponse('Authenticated '\
+									'successfully')
+			else:
+				return HttpResponse('Disabled account')
+		else:
+			return HttpResponse('Invalid login')
+	else:
+		form = LoginForm()
+	return render(request, 'login/login.html',{'form':form})
+
+def customlogin(request):
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			username = cd['login']
+			password = cd['password']
+			try:
+				user = User._deflaut_manager.get_by_natural_key(username)
+			except User.DoesNotExist:
+				return None
